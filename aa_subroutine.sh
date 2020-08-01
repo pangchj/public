941613336b6bf16f6e6e977f06300a6386330d66
pangchj <58257578+pangchj@users.noreply.github.com>
2020-08-01 21:03:54 +0800

    Update aa_subroutine.sh

--- aa_subroutine.sh
+++ aa_subroutine.sh
@@ -4,4 +4,6 @@ sed -e "s/<path/<path\ fill='#ffff00'\ /g" $x > temp$x
 mv temp$x $x
 sed -e "s/#000000/#ffff00/g" $x > temp$x
 mv temp$x $x
+sed -e "s/fill:black/fill:yellow/g" $x > temp$x
+mv temp$x $x
 done
