8a5873c414b00f6846d830783b09fa95e6c60bd9
pangchj <58257578+pangchj@users.noreply.github.com>
2020-08-02 22:12:31 +0800

    Delete aa_subroutine.sh

--- aa_subroutine.sh
+++
@@ -1,9 +0,0 @@
-for x in $*
-do
-sed -e "s/<path/<path\ fill='#ffff00'\ /g" $x > temp$x
-mv temp$x $x
-sed -e "s/#000000/#ffff00/g" $x > temp$x
-mv temp$x $x
-sed -e "s/fill:black/fill:yellow/g" $x > temp$x
-mv temp$x $x
-done
