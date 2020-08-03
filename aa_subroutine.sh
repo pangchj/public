e030e6812fce9000d733b4fda91ed93a00fb27d3
pangchj <58257578+pangchj@users.noreply.github.com>
2020-08-03 20:56:23 +0800

    Update aa_subroutine.sh
    Better shell script that allows for more editing of icons.

--- aa_subroutine.sh
+++ aa_subroutine.sh
@@ -1,9 +1,8 @@
+first_var=$1
+shift
 for x in $*
 do
-sed -e "s/<path/<path\ fill='#ffff00'\ /g" $x > temp$x
-mv temp$x $x
-sed -e "s/#000000/#ffff00/g" $x > temp$x
-mv temp$x $x
-sed -e "s/fill:black/fill:yellow/g" $x > temp$x
-mv temp$x $x
+#echo $first_var $x
+sed -e "$first_var" $x > t1$x
+mv t1$x $x
 done
