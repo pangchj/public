8f85971eb64cba7a7e90ea02975173ffd8877813
pangchj <58257578+pangchj@users.noreply.github.com>
2020-08-04 21:19:09 +0800

    Filtering off blue color, plus other major updates
    Included code that will filter off blue color from icons (both png and selected svgz). Code also cuts down on repetition.

--- aa_change.sh
+++ aa_change.sh
@@ -6,14 +6,13 @@
 #  2. Set both files to executable by "chmod +x aa_change.sh" and "chmod +x aa_subroutine.sh"
 #  3. Run this script with "sudo aa_change.sh"
 
-
 # It is assumed that the images of your lyx files are in the following folder. Do change it if otherwise.
 dir_lyx_img=/usr/share/lyx/images
 
 # If you want to change the Math/ IPA/ Unicode icons to a different color, feel free to adjust the aa_subroutine.sh script.
 
 # Start of script to create the dark theme icons.
-dir_of_lyx_dt_sh=$PWD
+dir_cur=$PWD
 
 #####
 
@@ -22,66 +21,61 @@ echo "Copied original images into folder:" $dir_lyx_img"/../images_backup"
 
 #####
 
-find $dir_lyx_img/ -type f -name "*.png" -exec convert {} -negate {} \;
-echo "Inverted png images in classic icon set"
+# To negate only, use
+# find $dir_lyx_img/ -type f -name "*.png" -exec convert {} -negate {} \;
+
+find $dir_lyx_img/ -type f -name "*.png" -exec convert {} -negate -channel Blue -evaluate set 0 {} \;
+echo "Inverted png images and removed blue channel in classic icon set"
 
 #####
 
+pat0="s/<path/<path\ fill='#ffff00'\ /g ;s/#000000/#ffff00/g"
+
 cd $dir_lyx_img/math
 gunzip -S z *.svgz
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/<path/<path\ fill='#ffff00'\ /g ;s/#000000/#ffff00/g" *.svg
+$dir_cur/aa_subroutine.sh "$pat0" *.svg
 gzip -S z *.svg
 echo "Colored images in" $dir_lyx_img"/math/"
 
 ####
 
+pat1=$pat0"; s/fill:black/fill:yellow/g"
+
 cd $dir_lyx_img/ipa
 gunzip -S z *.svgz
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/<path/<path\ fill='#ffff00'\ /g ;s/#000000/#ffff00/g; s/fill:black/fill:yellow/g" *.svg
+$dir_cur/aa_subroutine.sh "$pat1" *.svg
 gzip -S z *.svg
 echo "Colored images in" $dir_lyx_img"/ipa/"
 
 #####
 
 cd $dir_lyx_img
+
 rm ./banner.svgz
 
-gunzip -S z special*.svgz
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/<path/<path\ fill='#ffff00'\ /g ;s/#000000/#ffff00/g; s/fill:black/fill:yellow/g" special*.svg
-gzip -S z special*.svg
-
-gunzip -S z info\-insert.svgz
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/<path/<path\ fill='#ffff00'\ /g ;s/#000000/#ffff00/g; s/fill:black/fill:yellow/g" info\-insert.svg
-gzip -S z info\-insert.svg
-
-gunzip -S z scri*.svgz
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/<path/<path\ fill='#ffff00'\ /g ;s/#000000/#ffff00/g; s/fill:black/fill:yellow/g" scri*.svg
-gzip -S z scri*.svg
+gunzip -S z special*.svgz info\-insert.svgz scri*.svgz
+$dir_cur/aa_subroutine.sh "$pat1" special*.svg info\-insert.svg scri*.svg
+gzip -S z special*.svg info\-insert.svg scri*.svg
 
 ## Math icons are experimental: More can be done to make the output look better.
 
-gunzip -S z math*.svgz
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/<path/<path\ fill='#ffff00'\ /g ;s/#000000/#ffff00/g; s/fill:black/fill:yellow/g" math*.svg
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/#800000/#ff8000/g; s/#008000/#80ff00/g; s/#000080/#ffff00/g; s/#323232/#ffff00/g" math*.svg
-gzip -S z math*.svg
+pat2=$pat1"; s/#800000/#ff8000/g; s/#008000/#80ff00/g; s/#000080/#ffff00/g; s/#323232/#ffff00/g"
 
-gunzip -S z ert\-insert.svgz
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/<path/<path\ fill='#ffff00'\ /g ;s/#000000/#ffff00/g; s/fill:black/fill:yellow/g" ert\-insert*.svg
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/#800000/#ff8000/g; s/#008000/#80ff00/g; s/#000080/#ffff00/g; s/#323232/#ffff00/g" ert\-insert*.svg
-gzip -S z ert\-insert*.svg
-
-gunzip -S z dialog\-show_ma*.svgz
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/<path/<path\ fill='#ffff00'\ /g ;s/#000000/#ffff00/g; s/fill:black/fill:yellow/g" dialog\-show_ma*.svg
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/#800000/#ff8000/g; s/#008000/#80ff00/g; s/#000080/#ffff00/g; s/#323232/#ffff00/g" dialog\-show_ma*.svg
-gzip -S z dialog\-show_ma*.svg
+gunzip -S z math*.svgz ert\-insert.svgz dialog\-show_ma*.svgz
+$dir_cur/aa_subroutine.sh "$pat2" math*.svg ert\-insert*.svg dialog\-show_ma*.svg
+gzip -S z math*.svg ert\-insert*.svg dialog\-show_ma*.svg
 
 # Invert colors for icons related to tables
 # (The SVGZ files can be scaled well. The PNG files cannot.)
 # (Also, the classic icon set does not include some icons.)
-# NOTE: The color scheme was designed so that the colors for the icons tabular\-feature_move*.svg can be inverted properly. 
-#       I changed to *.svg later so that the other icons are partially inverted, with mixed results.
+# The colors were chosen to invert colors of the icons tabular\-feature_move*.svg files
 gunzip -S z *.svgz
-$dir_of_lyx_dt_sh/aa_subroutine.sh "s/#dae3ea/#251c15/g; s/#e9eef2/#16110d/g; s/#a5bbd3/#5a442c/g; s/#ffffff/#000000/g; s/#023372/#fdcc8d/g; s/#16477c/#e9b883/g; s/#d6e9f8/#291607/g; s/#c1ddf5/#3e220a/g; s/#84abf4/#7b540b/g; s/#6e757b/#918a84/g; s/#84abf4/#7b540b/g; s/#99a2a9/#665d56/g; s/#1a4f8b/#e5b074/g; s/#8db1cc/#724e33/g" *.svg
+
+# This code inverts colors
+#$dir_cur/aa_subroutine.sh "s/#dae3ea/#251c15/g; s/#e9eef2/#16110d/g; s/#a5bbd3/#5a442c/g; s/#ffffff/#000000/g; s/#023372/#fdcc8d/g; s/#16477c/#e9b883/g; s/#d6e9f8/#291607/g; s/#c1ddf5/#3e220a/g; s/#84abf4/#7b540b/g; s/#6e757b/#918a84/g; s/#84abf4/#7b540b/g; s/#99a2a9/#665d56/g; s/#1a4f8b/#e5b074/g; s/#8db1cc/#724e33/g" *.svg
+
+# This code inverts colors and removes all blue.
+$dir_cur/aa_subroutine.sh "s/#dae3ea/#251c00/g; s/#e9eef2/#161100/g; s/#a5bbd3/#5a4400/g; s/#ffffff/#000000/g; s/#023372/#fdcc00/g; s/#16477c/#e9b800/g; s/#d6e9f8/#291600/g; s/#c1ddf5/#3e2200a/g; s/#84abf4/#7b5400b/g; s/#6e757b/#918a00/g; s/#84abf4/#7b5400/g; s/#99a2a9/#665d006/g; s/#1a4f8b/#e5b000/g; s/#8db1cc/#724e00/g" *.svg
 gzip -S z *.svg 
 
 # Replace some icons in the Classic *.png set with those that we colored earlier.
